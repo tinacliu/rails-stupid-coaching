cf9bbbb5330dcf059938bba8d5a5cdfa75702b42
Tina Liu <tina.chang.liu@gmail.com>
2019-05-20 14:14:48 +0900

    refactored code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,5 +5,12 @@ class QuestionsController < ApplicationController
   def answer
     # raise
     @answer = params[:question]
+
+    @coach_response = "I don't care, get dressed and go to work!"
+    if @answer == 'I am going to work'
+      @coach_response = 'Great!'
+    elsif @answer[-1] == '?'
+      @coach_response = 'Silly question, get dressed and go to work!'
+    end
   end
 end
